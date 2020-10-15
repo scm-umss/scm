73ae35a8cd43324fcba595c86553ff1a5c9e4295
Alejandro Guzman <alejandro.sawers@gmail.com>
2020-10-14 22:34:37 -0400

    [IMP] Mejoras en testing de acceso a Citas

--- tests/Browser/CitasTest.php
+++ tests/Browser/CitasTest.php
@@ -15,41 +15,80 @@ class CitasTest extends DuskTestCase
      *
      * @return void
      */
-    public function acceso_citas_como_admin()
+    public function test_acceso_citas_como_admin()
     {
         $this->seed();
 
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(1))
-                ->visit('/citas')
+                ->visit('/citas/pendientes')
                 ->assertSee('Citas Pendientes')
+                ->assertSee('Ver')
+                ->assertSee('Editar')
+                ->assertSee('Confirmar')
+                ->assertSourceHas('<input type="submit" value="Cancelar" class="btn btn-sm btn-danger">')
                 ->screenshot('CitasTest_acceso_citas_como_admin_1')
+                ->visit('/citas/confirmadas')
+                ->assertSee('Citas Confirmadas')
+                ->assertSee('Ver cita')
+                ->assertSourceHas('<input type="submit" value="Cancelar" class="btn btn-sm btn-danger">')
+                ->screenshot('CitasTest_acceso_citas_como_admin_2')
+                ->visit('/citas/historial')
+                ->assertSee('Historial de citas')
+                ->assertSee('Ver')
+                ->screenshot('CitasTest_acceso_citas_como_admin_3')
                 ;
         });
     }
 
-    public function acceso_citas_como_medico()
+    public function test_acceso_citas_como_medico()
     {
         $this->seed();
 
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(2))
-                ->visit('/citas')
-                ->assertSee('vista para medico')
+                ->visit('/citas/pendientes')
+                ->assertSee('Citas Pendientes')
+                ->assertDontSee('Ver')
+                ->assertDontSee('Editar')
+                ->assertSee('Confirmar')
+                ->assertSourceHas('<input type="submit" value="Cancelar" class="btn btn-sm btn-danger">')
                 ->screenshot('CitasTest_acceso_citas_como_medico_1')
+                ->visit('/citas/confirmadas')
+                ->assertSee('Citas Confirmadas')
+                ->assertDontSee('Ver cita')
+                ->assertSourceHas('<input type="submit" value="Cancelar" class="btn btn-sm btn-danger">')
+                ->screenshot('CitasTest_acceso_citas_como_medico_2')
+                ->visit('/citas/historial')
+                ->assertSee('Historial de citas')
+                ->assertSee('Ver')
+                ->screenshot('CitasTest_acceso_citas_como_medico_3')
                 ;
         });
     }
 
-    public function acceso_citas_como_paciente()
+    public function test_acceso_citas_como_paciente()
     {
         $this->seed();
 
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(3))
-                ->visit('/citas')
-                ->assertSee('vista para paciente')
+                ->visit('/citas/pendientes')
+                ->assertSee('Citas Pendientes')
+                ->assertDontSee('Ver')
+                ->assertDontSee('Editar')
+                ->assertDontSee('Confirmar')
+                ->assertSourceHas('<input type="submit" value="Cancelar" class="btn btn-sm btn-danger">')
                 ->screenshot('CitasTest_acceso_citas_como_paciente_1')
+                ->visit('/citas/confirmadas')
+                ->assertSee('Citas Confirmadas')
+                ->assertDontSee('Ver cita')
+                ->assertSourceHas('<input type="submit" value="Cancelar" class="btn btn-sm btn-danger">')
+                ->screenshot('CitasTest_acceso_citas_como_paciente_2')
+                ->visit('/citas/historial')
+                ->assertSee('Historial de citas')
+                ->assertSee('Ver')
+                ->screenshot('CitasTest_acceso_citas_como_paciente_3')
                 ;
         });
     }
