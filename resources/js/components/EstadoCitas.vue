d467e882f705e691011c3109e8c56116c29bc101
Jhonny D <jhodeht@gmail.com>
2020-11-04 22:07:47 -0400

    [FIX] elimando boton

--- resources/js/components/EstadoCitas.vue
+++ resources/js/components/EstadoCitas.vue
@@ -13,7 +13,6 @@
       </div>
     <!-- <highcharts :options="chartOptions" :callback="cargar" ref="chart"></highcharts> -->
     <highcharts :options="chartOptions"></highcharts>
-    <!-- <button @click="cargar">Cargar</button> -->
   </div>
 </template>
 
